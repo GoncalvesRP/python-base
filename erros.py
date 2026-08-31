7161b4ba008fc4318982d5f5fa141b02fa2da656
GoncalvesRP <rogerpg_sp@yahoo.com.br>
2026-08-31 05:28:23 -0300

    Attribute Errors

--- erros.py
+++ erros.py
@@ -6,18 +6,16 @@ import sys
 # (É mais facil pedir perdao do que permissão)
 
 try:
-    names = open("names.txt").readlines() # FileNotFoundError
-    1 / 1 # ZeroDivisionError
-    print(names.append) # AttributeError
-except FileNotFoundError:
-    print("[Error] File names.txt not found")
+    names = open("names.txt").readlines() 
+    # FileNotFoundError
+except FileNotFoundError as e:
+    print(f"{str(e)}.")
     sys.exit(1)
-except ZeroDivisionError:
-    print("[Error] You cant divide by zero!!!")
-    sys.exit(1)
-except AttributeError:
-    print("[Error] List doesn´t have banana")
-    sys.exit(1)
+    # TODO: Usar retry
+else:
+    print("Sucesso!!")
+finally:
+    print("Execute isso sempre!!")
 
 try:
     print(names[2])
